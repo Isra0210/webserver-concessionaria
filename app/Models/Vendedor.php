1f8ec070a119990a42630e54112602e9bc54db07
Israel Rodrigues <r.israel0210@gmail.com>
2026-09-24 18:45:53 -0300

    style: padroniza formatacao e indentacao do codigo

--- app/Models/Vendedor.php
+++ app/Models/Vendedor.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Models;
 
 class Vendedor
@@ -12,9 +13,24 @@ class Vendedor
         'comissao'  => ['required', 'numeric'],
     ];
 
-    public static function todos(): array { return Store::all(self::COLECAO); }
-    public static function buscar(int $id): ?array { return Store::find(self::COLECAO, $id); }
-    public static function criar(array $dados): int { return Store::insert(self::COLECAO, $dados); }
-    public static function atualizar(int $id, array $dados): bool { return Store::update(self::COLECAO, $id, $dados); }
-    public static function excluir(int $id): bool { return Store::delete(self::COLECAO, $id); }
+    public static function todos(): array
+    {
+        return Store::all(self::COLECAO);
+    }
+    public static function buscar(int $id): ?array
+    {
+        return Store::find(self::COLECAO, $id);
+    }
+    public static function criar(array $dados): int
+    {
+        return Store::insert(self::COLECAO, $dados);
+    }
+    public static function atualizar(int $id, array $dados): bool
+    {
+        return Store::update(self::COLECAO, $id, $dados);
+    }
+    public static function excluir(int $id): bool
+    {
+        return Store::delete(self::COLECAO, $id);
+    }
 }
